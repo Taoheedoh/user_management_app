74ca5b50d4ace8069196cbaa59c5c783d93a6bcd
Taoheedoh <taoheedoh@gmail.com>
2025-06-04 17:45:57 +0100

    database

---
+++ lib/db_helper/database_connection.dart
@@ -0,0 +1,22 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+
+class DatabaseConnection {
+  Future<Database> setDatabase() async {
+    var directory = await getApplicationDocumentsDirectory();
+    var path = join(directory.path, 'user_db');
+    var database = await openDatabase(
+      path,
+      version: 1,
+      onCreate: _createDatabase,
+    );
+    return database;
+  }
+
+  Future<void> _createDatabase(Database database, int version) async {
+    String sql =
+        "CREATE TABLE users (id INTEGER PRIMARY KEY, name TEXT, contact TEXT, description TEXR";
+    await database.execute(sql);
+  }
+}
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueAccent,
+        centerTitle: true,
+        title: Text("SQLite CRUD APP", style: TextStyle(color: Colors.white)),
+      ),
+
+      body: Container(),
+    );
+  }
+}
